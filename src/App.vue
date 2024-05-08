167ebee5b8ebf5a44853ac3c25fbb1bfedce7ac8
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-08 13:55:36 +0200

    add: scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
+<!-- *Milestone 0:
+Progettare la struttura del global state sulla linea degli esercizi svolti nei giorni precedenti. -->
+
 <script>
+import { store } from './store';
 export default {
   data() {
     return {
-
+      store,
     }
   }
 }
 </script>
 
 <template>
-<h1>ciao</h1>
+<h1>Ciao</h1>
 </template>
 
 <style lang="scss">
